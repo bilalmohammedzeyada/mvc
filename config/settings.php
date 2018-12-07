c61e10770f83effbd40eb08125ac89a3febf32a2
immingmar <immingmar@gmail.com>
2018-12-07 19:02:08 +0545

    Initial Commit

---
+++ config/settings.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: immingmar
+ * Date: 12/7/18
+ * Time: 6:41 PM
+ */
+    $setting = [
+        'url' => 'http://localhost/mvc',
+        'db_name' => '',
+        'db_user' => '',
+        'db_pass' => '',
+        'db_host' => '',
+        'timezone' => 'Asia/Kathmandu'
+    ];
